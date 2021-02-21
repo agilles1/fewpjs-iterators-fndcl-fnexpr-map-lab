b24681ac1c3481ca0b139dc3c6bf54b3f4fc360b
agilles1 <adam.g.tpt@gmail.com>
2021-02-20 22:23:09 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCase)
+}
+
+function titleCase(str) {
+  return str.split(' ').map((word) => {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
 }
